8371cc99dc3b436a9679f1c3333e9b1abe1b764d
toxaw <toxawest@gmail.com>
2025-01-20 20:30:17 +0200

    - updated news content parser

--- src/components/Game/news.js
+++ src/components/Game/news.js
@@ -9,12 +9,25 @@ const News = () => {
     const {settings: {currentLang}} = getFromStorage('config')
 
     useEffect(() => {
-        fetch(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${game.steamId}&count=8&maxlength=400&l=${currentLang}&format=json`)
+        fetch(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${game.steamId}&count=8&l=${currentLang}&format=json`)
             .then(res => res.json())
             .then(({appnews: {newsitems}}) => {
+                console.log(newsitems);
                 setData(newsitems)
             })
     }, [])
+
+    const editorParser = (c) => c
+        .replaceAll(/\r/gm, '')
+        .replaceAll(/\n/gm, `<br/>`)
+        .replaceAll(/\[url=(.*?)](.*?)\[\/url]/gm, '<a href="$1">$2</a>')
+        .replaceAll(/\[\*](.*?)\[\*]/gm, `$1</li><li>`)
+        .replaceAll(/\[list](.*?)\[\/list]/gm, `<ul><li>$1</ul></li>`)
+        .replaceAll(/\[b](.*?)\[\/b]/gm, `<b>$1</b>`)
+        .replaceAll(/\[h2](.*?)\[\/h2]/gm, `<h2>$1</h2>`)
+        .replaceAll(/\[i](.*?)\[\/i]/gm, `<i>$1</i>`)
+        .replaceAll(/\[img](.*?)\[\/img]/gm, `<img src="$1" style="max-width: 100%;"/>`)
+        .replaceAll(/\{STEAM_CLAN_IMAGE}/gm, 'https://clan.fastly.steamstatic.com/images/')
 
     const renderItem = (item) => {
         return (
@@ -26,8 +39,7 @@ const News = () => {
                              title={new Date(item.date * 1000).toLocaleDateString()}>{new Date(item.date * 1000).toLocaleDateString()}</time>
 
                 </div>
-                <div
-                    dangerouslySetInnerHTML={{__html: item.contents.replace(/\{STEAM_CLAN_IMAGE}/gm, 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans/')}}/>
+                <div dangerouslySetInnerHTML={{__html: editorParser(item.contents)}}/>
             </li>
         )
     }
