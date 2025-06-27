fad53bc6a4fc23f5323ac55677c38ba3611bd922
toxaw <toxawest@gmail.com>
2025-06-27 23:02:12 +0300

    - update local playtime if getting statistic from launcher

--- src/components/Game/content.jsx
+++ src/components/Game/content.jsx
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import {getFromStorage} from "../../helpers/getFromStorage";
+import {getFromStorage, setToStorage} from "../../helpers/getFromStorage";
 import RenderContent from "./renderContent";
 import {secondsToHms} from "../../hooks/usePlayTime";
 import {useEffect, useState} from "react";
@@ -13,12 +13,20 @@ const GameContent = () => {
     const ach = getFromStorage('achievements')[id]
 
     useEffect(() => {
-        setPlayTime(getFromStorage('playTime')[game.id]);
-        setLastPlayed(getFromStorage('lastPlayed')[game.id]);
-        electronConnector.getPlayTime({source: game.source, id: (game.nspId || game.steamId), unofficial: game.unofficial}).then(d => {
+        const playTime = getFromStorage('playTime');
+        const lastPlayed = getFromStorage('lastPlayed');
+        setPlayTime(playTime[game.id]);
+        setLastPlayed(lastPlayed[game.id]);
+        electronConnector.getPlayTime({
+            source: game.source,
+            id: (game.nspId || game.steamId),
+            unofficial: game.unofficial
+        }).then(d => {
             if (d) {
                 setLastPlayed(d.lastPlayed)
+                setToStorage('lastPlayed', {...lastPlayed, [game.id]: d.lastPlayed})
                 setPlayTime(d.playTime)
+                setToStorage('playTime', {...playTime, [game.id]: d.playTime})
             }
         })
     }, [])
@@ -41,7 +49,7 @@ const GameContent = () => {
     }
 
     const checkVersion = (link) => {
-        if(link){
+        if (link) {
             return (<div style={{display: 'inline', cursor: 'pointer'}} onClick={(e) => {
                     electronConnector.CheckVersion(link).then(r => {
                         e.target.innerHTML = 'Version - ' + r;
@@ -92,7 +100,7 @@ const GameContent = () => {
     }, {
         label: 'Check Version',
         value: checkVersion(game.downloadLink)
-    },{
+    }, {
         label: 'Version',
         value: game.buildVersion
     }]}/>
