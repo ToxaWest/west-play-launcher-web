ca0c5919638b2a3cb9875a547b1d311be8dd8b8e
toxaw <toxawest@gmail.com>
2025-08-15 20:46:37 +0300

    - updated steam settings generation

--- src/helpers/GoldbergSteamEmulator.ts
+++ src/helpers/GoldbergSteamEmulator.ts
@@ -1,15 +1,11 @@
 const working = [
-    1310410, 1259420, 1601580, 1681430, 814380, 2277560, 534380, 1601570, 1182900
+    1310410, 1259420, 1601580, 1681430, 814380, 2277560, 534380, 1601570, 1182900, 1771300
 ]
 
 const workingWithIssues = {
-    1182900: {
-        disableDlc: true
-    }
 }
 
 const notWorking = [
-    1771300
 ]
 
 export {working, notWorking, workingWithIssues}
